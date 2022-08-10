b837a9dff2f84efd7000a706d36dc0dc0cb4e0c1
nhanlnt <nhanlnt@smartosc.com>
2022-08-10 16:18:07 +0700

    fix header component

--- src/components/headerComp/HeaderComp.vue
+++ src/components/headerComp/HeaderComp.vue
@@ -3,7 +3,7 @@
     <div
       class="container relative flex flex-wrap justify-between items-center mx-auto pb-3.5 lg:pb-[30px] after:content-[''] after:w-full after:h-[1px] after:bg-ha-secondary after:absolute after:left-0 after:bottom-0"
     >
-      <a href="/" class="flex items-center md:mr-[17px] lg:mr-[34px]">
+      <a href="/" class="flex items-center lg:mr-[17px] xl:mr-[34px]">
         <img
           src="../../assets/images/logo.png"
           class="w-10 h-10 object-contain"
@@ -18,7 +18,7 @@
         />
         <button
           @click="onOpenMenuBtnClick"
-          class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none"
+          class="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg lg:hidden hover:bg-gray-100 focus:outline-none"
         >
           <span class="sr-only">Open main menu</span>
           <svg
@@ -37,15 +37,15 @@
         </button>
       </div>
       <div
-        :class="`${headerMenuMobileClass} z-50 left-0 top-[62px] justify-start items-center w-full md:flex md:flex-1 md:order-1 md:left-[unset] md:top-[unset] md:z-[unset]`"
+        :class="`${headerMenuMobileClass} z-50 max-w-screen-md left-1/2 -translate-x-1/2 top-[62px] justify-start items-center w-full lg:flex lg:flex-1 lg:order-1 lg:left-[unset] lg:top-[unset] lg:z-[unset] lg:translate-x-0 lg:max-w-full`"
       >
         <ul
-          class="flex flex-col p-4 mt-4 mx-2 bg-gray-50 rounded-lg border border-gray-100 md:flex-row md:space-x-5 lg:space-x-10 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white"
+          class="flex flex-col p-4 mt-4 mx-2 bg-gray-50 rounded-lg border border-gray-100 lg:flex-row lg:space-x-5 xl:space-x-10 lg:mt-0 lg:text-sm lg:font-medium lg:border-0 lg:bg-white"
         >
           <li v-for="item in headerMenu" v-bind:key="item">
             <a
               :href="item.path"
-              class="block py-2 pr-4 pl-3 text-base font-medium hover:text-ha-primary rounded transition-all md:bg-transparent md:text-ha-gray-lightest md:p-0"
+              class="block py-2 pr-4 pl-3 text-base font-medium hover:text-ha-primary rounded transition-all lg:bg-transparent lg:text-ha-gray-lightest lg:p-0"
               aria-current="page"
               >{{ item.label }}</a
             >
